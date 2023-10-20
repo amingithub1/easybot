6eb2552977465a2982904720327a0d244ad92c47
Elyas Khatinzadeh <amin72e@gmail.com>
2023-10-21 00:00:54 +0330

    simple commands

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv').config()
-const {Telegraf} = require('telegraf')
-
+const {Telegraf, Markup} = require('telegraf')
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.command('start', async (ctx) => {
@@ -11,9 +10,21 @@ bot.command('start', async (ctx) => {
 
 
 bot.command('ok', async (ctx) => {
-    ctx.telegram.sendMessage(ctx.message.chat.id, "ok")
+    ctx.telegram.sendMessage(
+        ctx.message.chat.id,
+        "OK 2",
+        Markup
+        .keyboard(['/simple', '/inline', '/pyramid'])
+        .oneTime()
+        .resize()
+        )
         .then(message => console.log(message.message_id))
         .catch(err => console.log(err.message))
 })
 
+bot.command(["simple", "inline", "pyramid"], ctx => {
+    ctx.reply("Command")
+})
 bot.launch()
+
+
