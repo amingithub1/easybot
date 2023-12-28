77adf9e7211090518e026cb9dc3c5723ba1cbab2
elyaskhatinzadeh <124669887+elyaskhatinzadeh@users.noreply.github.com>
2023-12-29 01:02:23 +0330

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ client
     .setProject(process.env.YOUR_APPWRITE_PROJECT_ID) // Replace with your Appwrite project ID
     .setKey(process.env.YOUR_APPWRITE_API_KEY); // Replace with your Appwrite API key
 
-
+var todoDatabase;
+var todoCollection;
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
@@ -26,8 +27,26 @@ bot.on('text', async (ctx) => {
     .database.createDocument('collections_id', { message })
     .then(() => console.log('Message saved to Appwrite'));
 
+     todoDatabase = await databases.create(
+        sdk.ID.unique(), 
+        'TodosDB'
+    );
+
+    todoCollection = await databases.createCollection(
+        todoDatabase.$id, 
+        sdk.ID.unique(), 
+        'Todos'
+    );
+    
+    await databases.createDocument(
+        todoDatabase.$id, 
+        todoCollection.$id, 
+        sdk.ID.unique(), 
+        message
+    );
+    
   // Example: Echo the received message
-  ctx.reply(`You said: ${message}`);
+  ctx.reply(`You said: ${message.text}`);
 });
 
 bot.launch();
