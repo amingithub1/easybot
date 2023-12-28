17b05611569cfbc8b03558a01729c582e2de2c77
elyaskhatinzadeh <124669887+elyaskhatinzadeh@users.noreply.github.com>
2023-12-29 00:58:06 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 require('dotenv').config()
 
-const appwrite = require('appwrite');
 const { Telegraf } = require('telegraf');
+const sdk = require("node-appwrite");
 
-const client = new appwrite.Client();
+const client = new sdk.Client();
+
 client
-  .setEndpoint('https://cloud.appwrite.io/v1')
-  .setProject(process.env.YOUR_APPWRITE_PROJECT_ID) // Replace with your Appwrite project ID
-  .setKey(process.env.YOUR_APPWRITE_API_KEY); // Replace with your Appwrite API key
+    .setEndpoint("https://cloud.appwrite.io/v1")
+    .setProject(process.env.YOUR_APPWRITE_PROJECT_ID) // Replace with your Appwrite project ID
+    .setKey(process.env.YOUR_APPWRITE_API_KEY); // Replace with your Appwrite API key
+
+
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
